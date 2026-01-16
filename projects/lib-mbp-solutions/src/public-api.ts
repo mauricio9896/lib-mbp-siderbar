a989c05c258ff9d6a7666fe96e72902bdd9a8a03
Mauricio Buitrago <mbuitrago@bankvision.com.co>
2026-01-16 12:08:55 -0500

    correcion de imports

--- projects/lib-mbp-solutions/src/public-api.ts
+++ projects/lib-mbp-solutions/src/public-api.ts
@@ -2,14 +2,5 @@
  * Public API Surface of lib-mbp-solutions
  */
 
-export * from './lib/sidebar/sidebar.component';
-export * from './lib/sidebar/sidebar.types';
-export * from './lib/auth/access.model';
-export * from './lib/auth/services/access-control.service';
-export * from './lib/auth/services/auth-session.service';
-export * from './lib/auth/services/auth.service';
-export * from './lib/auth/app-init';
-export * from './lib/auth/guards/auth.guard';
-export * from './lib/auth/guards/route-exists.guard';
-export * from './lib/auth/login/login.component';
-export * from './lib/auth/mocks/auth-login-response.mock';
+export * from './lib/sidebar';
+export * from './lib/auth';
